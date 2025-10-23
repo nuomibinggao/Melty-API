acd499c509c3cc5f7c42494d172aacc7edd0314d
nuomibinggao <Sicheng_Guide@hotmail.com>
2025-10-23 19:54:00 +0800

    fix build fail

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { uploadLevel } from "./routes/uploadLevel.js";
 import { getLevels } from "./routes/getLevels.js";
 import { verifyCode } from "./routes/access.js";
-import { exportLevels } from "./routes/exportLevels.js";
 
 export default {
   async fetch(request, env) {
@@ -17,10 +16,6 @@ export default {
 
     if (url.pathname === "/verify-code" && request.method === "POST") {
       return verifyCode(request, env);
-    }
-
-    if (url.pathname === "/export-levels" && request.method === "GET") {
-      return exportLevels(request, env);
     }
 
     return new Response("Not found", { status: 404 });
