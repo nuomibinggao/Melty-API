0aa2608534d6d597041b6a3962c47b980041ebbf
nuomibinggao <Sicheng_Guide@hotmail.com>
2025-10-31 21:54:54 +0800

    fix unicode chars error

--- src/routes/get-levels.js
+++ src/routes/get-levels.js
@@ -6,12 +6,19 @@ export async function getLevels(request, env) {
   const legacy = await env.DB.prepare("SELECT * FROM legacy_level LIMIT 1").all();
 
   // Convert results to JS string
+  function escapeUnicode(str) {
+    return str.replace(/[\u007F-\uFFFF]/g, function(ch) {
+      return '\\u' + ('0000' + ch.charCodeAt(0).toString(16)).slice(-4);
+    });
+  }
+
+  // Example
   const js = `
-export const comingSoonLevel = ${JSON.stringify(comingSoon.results[0] || null)};
-export const indieLevels = ${JSON.stringify(indie.results)};
-export const plcrLevels = ${JSON.stringify(plcr.results)};
-export const legacyLevel = ${JSON.stringify(legacy.results[0] || null)};
-`;
+  export const comingSoonLevel = ${escapeUnicode(JSON.stringify(comingSoon.results[0] || null))};
+  export const indieLevels = ${escapeUnicode(JSON.stringify(indie.results))};
+  export const plcrLevels = ${escapeUnicode(JSON.stringify(plcr.results))};
+  export const legacyLevel = ${escapeUnicode(JSON.stringify(legacy.results[0] || null))};
+  `;
 
   return new Response(js, {
     headers: {
