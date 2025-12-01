682cc6b6810fe8c41c7bbd406154bd7b45915b6b
nuomibinggao <Sicheng_Guide@hotmail.com>
2025-12-01 21:31:22 +0800

    get-levels endpoint now sorts levels by id descending

--- src/routes/get-levels.js
+++ src/routes/get-levels.js
@@ -2,8 +2,8 @@ export async function getLevels(request, env) {
   const db = env.DB;
 
   const comingSoon = await db.prepare("SELECT * FROM coming_soon LIMIT 1").first();
-  const indie = await db.prepare("SELECT * FROM indie_levels ORDER BY date DESC").all();
-  const plcr = await db.prepare("SELECT * FROM plcr_levels ORDER BY date DESC").all();
+  const indie = await db.prepare("SELECT * FROM indie_levels ORDER BY id DESC").all();
+  const plcr = await db.prepare("SELECT * FROM plcr_levels ORDER BY id DESC").all();
   const legacy = await db.prepare("SELECT * FROM legacy_level LIMIT 1").first();
 
   return {
