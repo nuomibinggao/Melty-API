8458f535c9c45ef377b1de794b13a0fb38f4b1b8
nuomibinggao <Sicheng_Guide@hotmail.com>
2026-04-21 22:49:04 +0800

    allow cors

---
+++ src/hooks.server.ts
@@ -0,0 +1,11 @@
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const response = await resolve(event);
+
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  return response;
+};
